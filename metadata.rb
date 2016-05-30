6468e5559d3e9582d7d5d5f1080a295fd4d9ba1d
Yuki Yugui Sonoda <yugui@gengo.com>
2016-05-30 15:56:37 +0900

    [skip ci] Correct URL

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'bazel'
 maintainer       'Gengo Inc.'
 maintainer_email 'gge@gengo.com'
-source_url       'github.com/gengo/cookbook-bazel'
-issues_url       'github.com/gengo/cookbook-bazel/issues'
+source_url       'https://github.com/gengo/cookbook-bazel'
+issues_url       'https://github.com/gengo/cookbook-bazel/issues'
 license          'BSD 3-Clause License'
 description      'Installs/Configures bazel'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
