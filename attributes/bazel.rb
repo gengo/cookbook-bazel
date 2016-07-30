318b476c8aa4afabc183e517ea58b29cc3ca4909
Yuki Yugui Sonoda <yugui@gengo.com>
2016-07-30 12:04:45 +0900

    Upgrade bazel from 0.3.0 to 0.3.1

--- attributes/bazel.rb
+++ attributes/bazel.rb
@@ -1,7 +1,7 @@
 include_attribute 'java'
 default.java.jdk_version = 8
 
-default.bazel.version = '0.3.0'
+default.bazel.version = '0.3.1'
 default.bazel.prefix = '/usr/local'
 default.bazel.installation_method = nil  # automatic
 
